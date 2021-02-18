95fb7fac8190267e6d0eec2d85ed8646bc557a7e
JoabMedel <65578244+JoabMedel@users.noreply.github.com>
2021-02-18 13:43:32 -0600

    relaciones modelos

--- src/models/resettokens.js
+++ src/models/resettokens.js
@@ -4,13 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class ResetTokens extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.Users,{
+        foreignKey:"userId"
+      })
     }
   };
   ResetTokens.init({
--- src/models/users.js
+++ src/models/users.js
@@ -4,13 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsToMany(models.Roles,{
+        through:"UserRoles",
+        foreignKey:"userId"
+      });
+      this.hasMany(models.ResetTokens,{
+        foreignKey:"userId"
+      })
     }
   };
   Users.init({
